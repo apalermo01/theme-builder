d1f5a7234c009358d9268268e73ef77fd54c6ddc
Alex Palermo <alex.palermo.45@gmail.com>
2024-11-15 07:17:38 -0500

    set up first pass at new nvim config

--- modules/nvim.py
+++ modules/nvim.py
@@ -14,6 +14,88 @@ def parse_nvim(template_dir: str,
                destination_dir: str,
                config: Dict,
                theme_name: str):
+    """
+    Example neovim configs:
 
+    vanilla neovim:
+    ```json
+    "nvim": {
+        "colorscheme": "gruvbox"
+    }
+    ```
+
+    vanilla neovim pointing the colorscheme to a custom file
+    ```json
+    "nvim": {
+        "colorscheme": {
+            "colorscheme": "gruvbox",
+            "file": "opt/options.lua"
+        }
+    }
+    ```
+    this will place the option for colorscheme inside
+    `<home directory>/.config/nvim/opt/options.lua`
+
+    nvchad:
+    ```json
+    "nvim": {
+        "template_dir": "default_configs/nvchad/"
+        "nvchad_colorscheme": "gruvbox"
+    }
+    ```
+    """
     logger.info("Loading nvim...")
     nvim_config: Dict = config.get('nvim', {})
+    if 'colorscheme' in nvim_config:
+        _configure_colorscheme(nvim_config, theme_name)
+    if 'nvchad_colorscheme' in nvim_config:
+        _configure_nvchad_colorscheme(nvim_config, theme_name)
+
+    if 'nvchad_separator' in nvim_config:
+        _configure_nvchad_separator(nvim_config, theme_name)
+
+
+def _configure_colorscheme(nvim_config: Dict, theme_name: str):
+    if isinstance(nvim_config['colorsheme'], str):
+        colorscheme: str = nvim_config['colorscheme']
+        colorscheme_path: str = os.path.join(
+            ".", "themes", theme_name, "dots", ".config", "nvim", "init.lua"
+        )
+    else:
+        colorscheme: str = nvim_config['colorscheme']['colorscheme']
+        colorscheme_path: str = os.path.join(
+            ".", "themes", theme_name, "dots", ".config", "nvim",
+            nvim_config['colorscheme']['file']
+        )
+        if not os.path.exists(os.path.split(colorscheme_path)[0]):
+            os.makedirs(os.path.split(colorscheme_path)[0])
+
+    cmd: str = f"vim.cmd[[colorscheme {colorscheme}]]"
+
+    overwrite_or_append_line(pattern="vim.cmd[[colorscheme",
+                             replace_text=cmd,
+                             dest=colorscheme_path)
+
+
+def _configure_nvchad_colorscheme(nvim_config: Dict, theme_name: str):
+    colorscheme: str = nvim_config['nvchad_colorscheme']
+    pattern: str = 'theme = "'
+    text: str = f'    theme = "{colorscheme}",'
+    path: str = os.path.join(
+        "themes", theme_name, "dots", ".config", "nvim", "lua", "chadrc.lua"
+    )
+    overwrite_or_append_line(path=path,
+                             pattern=pattern,
+                             replace_text=text)
+
+
+def _configure_nvchad_separator(nvim_config: Dict, theme_name: str):
+    separator: str = nvim_config['nvchad_separator']
+    pattern: str = 'separator_style = "'
+    text: str = f'       separator_style = "{separator}",'
+    path: str = os.path.join(
+        "themes", theme_name, "dots", ".config", "nvim", "lua", "chadrc.lua"
+    )
+    overwrite_or_append_line(path=path,
+                             pattern=pattern,
+                             replace_text=text)
