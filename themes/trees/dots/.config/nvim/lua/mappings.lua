b53cba298fe939189a56d18277f3a73d83544dd1
apalermo01 <64085614+apalermo01@users.noreply.github.com>
2025-01-18 15:53:01 -0500

    updated tmux theme on trees (#5)

--- themes/trees/dots/.config/nvim/lua/mappings.lua
+++ themes/trees/dots/.config/nvim/lua/mappings.lua
@@ -18,3 +18,7 @@ map("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>")
 map("n", "<leader>gpD", "<cmd>lua require('goto-preview').goto_preview_declaration()<CR>")
 map("n", "<leader>gP", "<cmd>lua require('goto-preview').close_all_win()<CR>")
 
+map('n', '<leader>ff', require('telescope.builtin').find_files, { desc = 'Telescope find files' })
+map('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = 'Telescope live grep' })
+map('n', '<leader>fb', require('telescope.builtin').buffers, { desc = 'Telescope buffers' })
+map('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = 'Telescope help tags' })
