15b76c65e58ff2225c433dc990c99eb4d5593dbd
Alex Palermo <alex.palermo.42@protonmail.com>
2025-01-18 23:10:54 -0500

    added fastfetch as an option

--- themes/i3_catppuccin/dots/.config/fish/config.fish
+++ themes/i3_catppuccin/dots/.config/fish/config.fish
@@ -5,5 +5,5 @@ end
 export PATH="$HOME/.local/share/gem/ruby/3.0.0/gems/jekyll-4.3.3/exe:$PATH"
 export PATH="$HOME/.local/share/gem/ruby/3.0.0/bin:$PATH"
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/alex/.ghcup/bin # ghcup-env
-neofetch
+fastfetch
 wal -n -e -i /home/alex/Pictures/wallpapers/darker_unicat.png > /dev/null 
