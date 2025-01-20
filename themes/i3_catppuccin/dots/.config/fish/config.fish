4e7d42d3c4d7ea0c2b75c9aa0142f3a6ac09a568
Alex Palermo <alex.palermo.42@protonmail.com>
2025-01-20 12:27:15 -0500

    added roadmap

--- themes/i3_catppuccin/dots/.config/fish/config.fish
+++ themes/i3_catppuccin/dots/.config/fish/config.fish
@@ -6,6 +6,6 @@ export PATH="$HOME/.local/share/gem/ruby/3.0.0/gems/jekyll-4.3.3/exe:$PATH"
 export PATH="$HOME/.local/share/gem/ruby/3.0.0/bin:$PATH"
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/alex/.ghcup/bin # ghcup-env
 
-alias personal=bash ~/personal_docs.sh
-alias reading=bash ~/reading_session.sh
+abbr --add personal bash ~/personal_docs.sh
+abbr --add reading bash ~/reading_session.sh
 fastfetch
