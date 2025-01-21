582933d3faf510e3843efb2a8107c53464b99d92
Alex Palermo <alex.palermo.42@protonmail.com>
2025-01-21 07:17:49 -0500

    some script updates

--- themes/i3_catppuccin/scripts/configure_theme.fish
+++ themes/i3_catppuccin/scripts/configure_theme.fish
@@ -3,3 +3,4 @@ kitty +kitten themes --reload-in=all catppuccin-mocha
 fisher install catppuccin/fish
 fish_config theme save "Catppuccin Mocha"
 fish_config prompt save terlar
+bash ~/.config/polybar/i3_polybar_start.sh
