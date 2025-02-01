035b3b4edf34198d7a34a73eef7d45fe8d048518
Alex Palermo <alex.palermo.42@protonmail.com>
2025-02-01 12:29:06 -0500

    updated config for theme 000

---
+++ check-theme/dotfiles_backups/2025-02-01::12:27:47/.config/nvim/lua/chadrc.lua
@@ -0,0 +1,26 @@
+-- This file needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
+
+---@type ChadrcConfig
+local M = {}
+
+M.base46 = {
+    theme = "nord",
+
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
+}
+
+M.ui = {
+  statusline = {
+    theme = "default",
+    separator_style = "default"
+  },
+  nvdash = {
+    load_on_startup = true,
+  }
+
+}
+return M
