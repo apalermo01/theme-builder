016adde73b83a4f154d738e875d4bae838b066e8
Alex Palermo <alex.palermo.42@protonmail.com>
2025-02-17 21:25:21 -0500

    updated fish and i3 config based on recent updates

--- default_configs/fish/config.fish
+++ default_configs/fish/config.fish
@@ -17,8 +17,8 @@ function on -d "Create a new note for obsidian"
         set file_name $(echo $argv[1] | tr ' ' '-')
         set formatted_file_name $(date "+%Y-%m-%d")_$file_name.md
         cd $NOTES_PATH
-        touch "inbox/$formatted_file_name"
-        nvim "inbox/$formatted_file_name"
+        touch "0-inbox/$formatted_file_name"
+        nvim "0-inbox/$formatted_file_name"
         echo "file name: " $file_name
     else
         echo "Expected an argument!"
@@ -27,25 +27,30 @@ function on -d "Create a new note for obsidian"
 end
 
 function og -d "Move notes based on tags" 
-    set VAULTS craft personal
+    set VAULTS 6-full-notes 7-full-notes-personal
     
     for VAULT_NAME in $VAULTS
-        find "$NOTES_PATH/notes/$VAULT_NAME" -type f -name '*.md' | while read -l file;
-            echo "Processing $file"
+        find "$NOTES_PATH/2-notes/$VAULT_NAME" -type f -name '*.md' -not -path "*tags*"| while read -l file;
             set tag $(awk -F': ' '/^type:/{print $2; exit}' "$file" | sed -e 's/^ *//;s/ *$//')
             if [ ! -z "$tag" ]
-                echo "Found tag $tag"
-                set TARGET_DIR "$NOTES_PATH/notes/$VAULT_NAME/$tag" 
-
-                mkdir -p $TARGET_DIR
-                mv $file "$TARGET_DIR/" 
-                echo "Moved $file to $TARGET_DIR"
+                set TARGET_DIR "$NOTES_PATH/2-notes/$VAULT_NAME/$tag" 
+                
+                if [ $file != "$TARGET_DIR/$(path basename $file)" ]
+                    echo "Processing $(path basename $file)"
+                    echo "Found tag $tag"
+                    mkdir -p $TARGET_DIR
+                    mv $file "$TARGET_DIR/" 
+                    echo "Moved $file to $TARGET_DIR"
+                end
             else 
-                echo "No type tag found"
+                echo "No type tag found for $(path basename $file)"
             end
         end
     end
+
+    echo "vault restructure complete"
 end
+
 export PATH="$HOME/.local/share/gem/ruby/3.0.0/gems/jekyll-4.3.3/exe:$PATH"
 export PATH="$HOME/.local/share/gem/ruby/3.0.0/bin:$PATH"
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/alex/.ghcup/bin # ghcup-env
@@ -54,4 +59,4 @@ set -x MANPAGER "nvim +Man!"
 abbr --add personal bash ~/personal_docs.sh
 abbr --add reading bash ~/reading_session.sh
 abbr --add notes cd ~/Documents/git/notes/
-
+abbr --add zen /usr/local/bin/zen/zen
