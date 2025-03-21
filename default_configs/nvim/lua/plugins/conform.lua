850a1a5f604fa03ab2efc8d57aee267bc45fd256
Alex Palermo <alex.palermo.42@protonmail.com>
2025-03-21 14:41:37 -0400

    a few bugfixes

--- default_configs/nvim/lua/plugins/conform.lua
+++ default_configs/nvim/lua/plugins/conform.lua
@@ -1,30 +1,34 @@
 return {
 	"stevearc/conform.nvim",
-	opts = {
-		formatters_by_ft = {
-			lua = { "stylua" },
-			python = function(bufnr)
-				if require("conform").get_formatter_info("ruff_format", bufnr).available then
-					return { "ruff_format" }
-				else
-					return { "isort", "black" }
-				end
-			end,
-            go = { 'gofmt' },
-            json = { 'yq' },
-            md = { 'mdformat' },
-            sql = { 'pg_format' },
-            yaml = { 'yq' }
-
-		},
-	},
-
-	keys = {
+	opts = function()
+		return {
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = function(bufnr)
+					local conform = require("conform")
+					local formatter_info = conform.get_formatter_info("ruff_format", bufnr)
+					
+					if formatter_info and formatter_info.available then
+						return { "ruff_format" }
+					else
+						return { "isort", "black" }
+					end
+				end,
+				-- Uncomment these when needed
+				-- go = { "gofmt" },
+				-- json = { "yq" },
+				-- md = { "mdformat" },
+				-- sql = { "pg_format" },
+				-- yaml = { "yq" },
+			},
+		}
+	end,
+	keys = { {
 		"<leader>fm",
 		function()
 			require("conform").format({ lsp_fallback = true })
 		end,
 		"n",
 		desc = "general format file",
-	},
+	} },
 }
