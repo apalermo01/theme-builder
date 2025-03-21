dbc7c00fc619e1c6db56e81a5ebaefef898f9168
Alex Palermo <alex.palermo.42@protonmail.com>
2025-03-21 13:11:42 -0400

    some cmp and lsp changes

--- default_configs/nvim/lua/lsp.lua
+++ default_configs/nvim/lua/lsp.lua
@@ -1,6 +1,8 @@
 require("mason").setup()
 require("mason-lspconfig").setup()
 
+local border = 'single'
+
 local handlers = {
 	["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
 	["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border }),
