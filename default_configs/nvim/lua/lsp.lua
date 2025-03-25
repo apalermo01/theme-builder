f56478b08bc023114cd76d58706ac1cd2725a65f
Alex Palermo <alex.palermo.42@protonmail.com>
2025-03-25 07:56:48 -0400

    configuring lsps

--- default_configs/nvim/lua/lsp.lua
+++ default_configs/nvim/lua/lsp.lua
@@ -1,13 +1,18 @@
 require("mason").setup()
 require("mason-lspconfig").setup()
 
-local border = 'single'
+local border = "single"
 
 local handlers = {
 	["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
 	["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border }),
 }
-local servers = { "html", "cssls", "clangd", "pylsp", "ts_ls", "lua_ls", "gopls" }
+local servers = { "html", "cssls", "clangd", "pyright", "ts_ls", "lua_ls", "gopls" }
+local nvlsp = require("config.lspconfig")
+
 for _, lsp in ipairs(servers) do
-	require("lspconfig")[lsp].setup({ handlers = handlers })
+	require("lspconfig")[lsp].setup({
+		handlers = handlers,
+		on_attach = nvlsp.on_attach,
+	})
 end
--- themes/i3_catppuccin_macchiato/nvim/edulsp.lua
+++ themes/i3_catppuccin_macchiato/nvim/edulsp.lua
@@ -1,7 +1,16 @@
-local client = vim.lsp.start_client {
-    name = "educationalsp",
-    cmd = { "/home/alex/Documents/git/educationalsp/main"},
-}
+local client = vim.lsp.start_client({
+	name = "educationalsp",
+	cmd = { "/home/alex/Documents/git/educationalsp/main" },
+    on_attach = require("config.lspconfig").on_attach
+})
 
--- if not client then 
---     vim.notify
+if not client then
+	vim.notify("You didn't do the client thing good")
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "markdown",
+	callback = function()
+		vim.lsp.buf_attach_client(0, client)
+	end,
+})
