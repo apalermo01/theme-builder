05ab50c4cbc30f79f831c334b7e10b8d7cca8c03
Alex Palermo <alex.palermo.42@protonmail.com>
2025-03-29 16:14:46 -0400

    created gruvbox theme

---
+++ themes/i3_gruvtux/scripts/configure_theme.sh
@@ -0,0 +1,7 @@
+#!/bin/fish
+# kitty +kitten themes --reload-in=all catppuccin-macchiato
+fisher install jomik/fish-gruvbox
+fish_config prompt save acidhub
+bash ~/.config/polybar/i3_polybar_start.sh
+
+
