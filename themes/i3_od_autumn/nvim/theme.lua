420eb3c90040beeff9a66c7f7ff414d27d062195
Alex Palermo <alex.palermo.42@protonmail.com>
2025-03-29 07:50:04 -0400

    added tokyonight theme

--- themes/i3_od_autumn/nvim/theme.lua
+++ themes/i3_od_autumn/nvim/theme.lua
@@ -3,6 +3,11 @@ return {
     {
         "olimorris/onedarkpro.nvim",
         priority = 1000, -- Ensure it loads first
+        opts = {
+            options = {
+                highlight_inactive_windows = false
+            }
+        }
     },
     {
         "nvim-lualine/lualine.nvim",
---
+++ themes/i3_tokyonight/README.md
@@ -0,0 +1,3 @@
+# Tokyonight
+
+Wallpaper source: https://github.com/atraxsrc/tokyonight-wallpapers/blob/main/spookyjs_upscayl_realesrgan-x4plus_x2.png
