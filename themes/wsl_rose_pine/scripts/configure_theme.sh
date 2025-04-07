496fc3bab90203fbf3731fa7aef0787a87f63d71
Alex Palermo <alex.palermo@level.agency>
2025-04-07 14:52:14 -0400

    added rose pine for wsl

---
+++ themes/wsl_rose_pine/scripts/configure_theme.sh
@@ -0,0 +1,18 @@
+#!/bin/fish
+# fisher install rkbk60/onedark-fish
+# fish_config prompt save acidhub
+#
+# set -l onedark_options '-b'
+#
+# if set -q VIM
+#     # Using from vim/neovim.
+#     set onedark_options "-256"
+# else if string match -iq "eterm*" $TERM
+#     # Using from emacs.
+#     function fish_title; true; end
+#     set onedark_options "-256"
+# end
+#
+# set_onedark $onedark_options
+#
+#
