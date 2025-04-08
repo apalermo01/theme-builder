02e412ff4ff50cdda000d7a19fc23844ac800661
Alex Palermo <alex.palermo@level.agency>
2025-04-08 17:41:03 -0400

    tweaks to a few themes

--- themes/wsl_rose_pine/scripts/configure_theme.sh
+++ themes/wsl_rose_pine/scripts/configure_theme.sh
@@ -1,20 +1,2 @@
 #!/bin/fish
 
-echo "RUNNING ROSE PINE SCRIPT"
-# fisher install rkbk60/onedark-fish
-# fish_config prompt save acidhub
-#
-# set -l onedark_options '-b'
-#
-# if set -q VIM
-#     # Using from vim/neovim.
-#     set onedark_options "-256"
-# else if string match -iq "eterm*" $TERM
-#     # Using from emacs.
-#     function fish_title; true; end
-#     set onedark_options "-256"
-# end
-#
-# set_onedark $onedark_options
-#
-#
