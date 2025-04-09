2f149fcef4ed7b5836cb5a492a893b7d0580aced
Alex Palermo <alex.palermo@level.agency>
2025-04-09 13:15:48 -0400

    update fish config

--- themes/wsl_dracula/fish/config.fish
+++ themes/wsl_dracula/fish/config.fish
@@ -1,3 +1,4 @@
 
 set -U OBSIDIAN_NOTES_DIR /mnt/c/Users/apalermo/github/notes/
 set -U NOTES_DIR /mnt/c/Users/apalermo/github/notes/
+source ~/work_cmds.fish
--- themes/wsl_rose_pine/fish/config.fish
+++ themes/wsl_rose_pine/fish/config.fish
@@ -2,3 +2,4 @@
 set -U OBSIDIAN_NOTES_DIR /mnt/c/Users/apalermo/github/notes/
 set -U NOTES_DIR /mnt/c/Users/apalermo/github/notes/
 fish_config theme choose "Rosé Pine"
+source ~/work_cmds.fish
