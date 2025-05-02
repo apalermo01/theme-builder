a0bfbe62323828b2d802a60f3a1f8c603f4630b8
Alex <alex.palermo.ai@gmail.com>
2025-05-02 19:33:21 -0400

    Temporarily comment out image plugin

---
+++ default_configs/nvim/lua/config/lazy/image.lua
@@ -0,0 +1,14 @@
+return {
+    -- "3rd/image.nvim",
+    -- opts = {
+    --     backend = "kitty",
+    --     processor = "magick_cli",
+    --     resolve_image_path = function(document_path, image_path, fallback)
+    --         local working_dir = vim.fn.getcwd()
+    --         if (working_dir:find("$HOME/Documents/git/notes/")) then
+    --             return working_dir .. "/" .. image_path
+    --         end
+    --         return fallback(document_path, image_path)
+    --     end,
+    -- } 
+}
