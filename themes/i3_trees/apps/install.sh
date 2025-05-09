2babc5d74d65d32cfeb0f6d9a7b25094a38c4576
Alex <alex.palermo.ai@gmail.com>
2025-05-09 07:06:25 -0400

    Added plasma theme for everforest

---
+++ themes/i3_trees/apps/install.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+echo $(curl -L https://raw.githubusercontent.com/Serge2702/KDE-Everforest/refs/heads/main/Everforest.colors) > $HOME/.local/share/color-schemes/Everforest.colors
