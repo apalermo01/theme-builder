29d9a1c345fd881c2338472cd05fa8ded05f93ab
Alex Palermo <alex.palermo@level.agency>
2025-05-28 06:18:08 -0400

    nvim updates

--- default_configs/nvim/lua/config/lazy/lsp/cmp.lua
+++ default_configs/nvim/lua/config/lazy/lsp/cmp.lua
@@ -1,6 +1,5 @@
 -- auto complete
 
-
 -- `/` cmdline setup.
 local cmp = require("cmp")
 cmp.setup.cmdline("/", {
@@ -28,7 +27,7 @@ cmp.setup.cmdline(":", {
 return {
 	"hrsh7th/nvim-cmp",
 	dependencies = {
-        "onsails/lspkind.nvim",
+		"onsails/lspkind.nvim",
 		"stevearc/conform.nvim",
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
@@ -45,14 +44,17 @@ return {
 	},
 	config = function()
 		local cmp = require("cmp")
-        local lspkind = require("lspkind")
+		local lspkind = require("lspkind")
 		local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
 		cmp.setup({
 			snippet = {
 				expand = function(args)
 					require("luasnip").lsp_expand(args.body)
 				end,
 			},
+
+			preselect = "None",
 
 			formatting = {
 				format = lspkind.cmp_format({
@@ -72,13 +74,22 @@ return {
 
 			mapping = cmp.mapping.preset.insert({
 				["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+				["$"] = cmp.mapping.select_prev_item(cmp_select),
 				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+				["+"] = cmp.mapping.select_next_item(cmp_select),
+				["<C-k>"] = cmp.mapping.scroll_docs(-4),
+				["<C-j>"] = cmp.mapping.scroll_docs(4),
+				["<C-e>"] = cmp.mapping.abort(),
+				["<C-o>"] = cmp.mapping.open_docs(),
 				["<TAB>"] = cmp.mapping.select_next_item(cmp_select),
-				["<C-k>"] = cmp.mapping.select_prev_item(cmp_select),
 				["<S-TAB>"] = cmp.mapping.select_prev_item(cmp_select),
-				["<C-j>"] = cmp.mapping.select_next_item(cmp_select),
 				["<C-y>"] = cmp.mapping.confirm({ select = true }),
-				-- ["<CR>"] = cmp.mapping.confirm({ select = true }),
+
+				-- fn1+h/j/k/l mapped to arrow keys
+				-- fn1+y is mapped to >
+				-- also map > to confirm so I don't have to move to ctrl
+				[">"] = cmp.mapping.confirm({ select = true }),
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
 				["<C-space>"] = cmp.mapping.complete(),
 			}),
 
@@ -88,10 +99,86 @@ return {
 				{ name = "buffer" },
 				{ name = "render-markdown" },
 				{ name = "obsidian" },
-				-- { name = 'spell' },
+				{ name = "spell" },
 				-- { name = 'gitmoji' },
 				{ name = "path" },
 			}),
 		})
+
+		local api = vim.api
+		local lsp_util = vim.lsp.util
+
+		local help_lines = {
+			"Cmp mappings:",
+			"<C-p> • prev item",
+			"<C-n> • next item",
+			"<C-k>/<C-j> • scroll docs",
+			"<C-space> • trigger complete",
+			"<C-y> • confirm ",
+			"<C-e> • abort   ",
+			"<C-o> • open docs",
+		}
+
+		local help_buf = api.nvim_create_buf(false, true)
+		api.nvim_buf_set_option(help_buf, "bufhidden", "hide")
+		api.nvim_buf_set_lines(help_buf, 0, -1, false, help_lines)
+
+		local help_win
+		local close_timer
+
+		cmp.event:on("menu_opened", function()
+			vim.schedule(function()
+				if close_timer then
+					close_timer:stop()
+					close_timer = nil
+				end
+
+				if help_win and api.nvim_win_is_valid(help_win) then
+					return
+				end
+
+				local width = 0
+				local height = #help_lines
+
+				for _, line in ipairs(help_lines) do
+					width = math.max(width, #line)
+				end
+
+				local row = 0
+				local col = vim.o.columns - width
+
+				vim.notify("making cmp window. row = " .. row .. " col = " .. col, vim.log.levels.WARN)
+				-- open the window
+                help_win = api.nvim_open_win(help_buf, false, {
+                    relative = 'editor',
+                    row = row,
+                    col = col,
+                    width = width,
+                    height = height,
+                    style = 'minimal',
+                    border = 'rounded',
+                })
+				-- help_win = lsp_util.open_floating_preview(help_lines, "plaintext", {
+				-- 	relative = "editor",
+				-- 	row = row,
+				-- 	col = col,
+				-- 	width = width,
+				-- 	height = height,
+				-- 	style = "minimal",
+				-- 	border = "rounded",
+				-- })
+			end)
+		end)
+
+		cmp.event:on("menu_closed", function()
+			if help_win and api.nvim_win_is_valid(help_win) then
+				close_timer = vim.defer_fn(function()
+					if help_win and api.nvim_win_is_valid(help_win) then
+						api.nvim_win_close(help_win, true)
+						help_win = nil
+					end
+				end, 10000)
+			end
+		end)
 	end,
 }
