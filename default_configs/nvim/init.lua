eb495fde74fe8fecfd42e7f2881e89d1bc9df619
Alex <alex.palermo.ai@gmail.com>
2025-06-07 17:31:46 -0400

    some fixes to env vars in nvim

--- default_configs/nvim/init.lua
+++ default_configs/nvim/init.lua
@@ -1,5 +1,5 @@
 -- Globals
-OBSIDIAN_NOTES_DIR = os.getenv("OBSIDIAN_NOTES_DIR") or "/home/alex/Documents/git/notes"
+OBSIDIAN_NOTES_DIR = os.getenv("NOTES_PATH") or "/home/alex/Documents/git/notes"
 OBSIDIAN_NOTES_SUBDIR = os.getenv("OBSIDIAN_NOTES_SUBDIR") or "0-inbox"
 OBSIDIAN_TEMPLATE_FOLDER = os.getenv("OBSIDIAN_TEMPLATE_FOLDER") or "5-templates"
 
--- master_font.sh
+++ master_font.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash 
 export FONT="Iosevka Nerd Font Mono"
-export FONTSIZEPX=16
-
 # FONT="JetBrainsMono Nerd Font"
 # FONT="FiraCode Nerd Font Mono"
+export FONTSIZEPX=16
